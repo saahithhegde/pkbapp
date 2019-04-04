e568dac1a292735ff6502c8ddd5fe4416e5d80e8
datron <kartik54321@hotmail.com>
2019-04-04 12:16:52 +0530

    added icons and xml. Connecting camera next.

--- app/src/main/java/sdi/com/pkb/MainActivity.java
+++ app/src/main/java/sdi/com/pkb/MainActivity.java
@@ -1,7 +1,8 @@
 package sdi.com.pkb;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
